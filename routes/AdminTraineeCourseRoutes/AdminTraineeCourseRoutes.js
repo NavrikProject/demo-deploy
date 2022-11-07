430ad168df1516716f5234c98db27153ed7da2da
Mahesh <b.mahesh311296@gmail.com>
2022-11-07 18:13:54 +0530

    changes mentor reg

--- routes/AdminTraineeCourseRoutes/AdminTraineeCourseRoutes.js
+++ routes/AdminTraineeCourseRoutes/AdminTraineeCourseRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   bookInstructorLiveClass,
   getAllBookingLiveClasses,
+  getAllTraineeCompleteCourses,
+  getTraineeCompletedLiveClassDetailsInAdmin,
   getTraineeInCompleteCourses,
   getTraineeLiveClassDetailsInAdmin,
 } from "../../controllers/AdminTraineeCourseControllers/AdminTraineeCourseControllers.js";
@@ -13,5 +15,10 @@ router.get("/get/all/instructor-class", getAllBookingLiveClasses);
 router.post(
   "/get/individual/instructor-class",
   getTraineeLiveClassDetailsInAdmin
+);
+router.get("/get/all/completed-courses", getAllTraineeCompleteCourses);
+router.post(
+  "/get/all/live-completed-classes",
+  getTraineeCompletedLiveClassDetailsInAdmin
 );
 export default router;
